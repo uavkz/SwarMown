6cae37d80699b151f2c53f348cff148e984ad404
Yakunin Kirill <yakunin.k@mail.ru>
2020-03-06 01:36:07 +0600

    Field - draw grid

--- mainapp/services_draw.py
+++ mainapp/services_draw.py
@@ -1,3 +1,6 @@
+from shapely.geometry import Point
+from shapely.geometry.polygon import Polygon
+
 def get_field():
     points = [
         [150, 120],
@@ -6,4 +9,20 @@ def get_field():
         [900, 700],
         [300, 750],
     ]
-    return [coord for point in points for coord in point]
+    return points
+
+
+def get_grid(field, step):
+    grid = []
+    min_x = min((p[0] for p in field))
+    min_y = min((p[1] for p in field))
+    max_x = max((p[0] for p in field))
+    max_y = max((p[1] for p in field))
+    polygon = Polygon(field)
+
+    for x in range(min_x, max_x, step):
+        for y in range(min_y, max_y, step):
+            point = Point(x, y)
+            if polygon.contains(point):
+                grid.append([x, y])
+    return grid
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,6 +1,6 @@
 from django.views.generic import TemplateView
 
-from mainapp.services_draw import get_field
+from mainapp.services_draw import get_field, get_grid
 
 
 class MownView(TemplateView):
@@ -8,5 +8,7 @@ class MownView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['field'] = get_field()
+        field = get_field()
+        context['field'] = [coord for point in field for coord in point]
+        context['grid'] = get_grid(field, 100)
         return context
