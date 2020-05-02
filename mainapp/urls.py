0dcccae3432efe1681a11f33b022da933b076a28
Sanzhar <sanzharmrz@gmail.com>
2020-05-02 21:18:40 +0600

    feat urls: add zamboni routing

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -6,5 +6,6 @@ app_name = 'mainapp'
 
 urlpatterns = [
     path('', MownView.as_view(), name="index"),
-    path('genetic/', GaView.as_view(), name='genetic')
+    path('genetic/', GaView.as_view(), name='genetic'),
+    path('zamboni/', ZamboniView.as_view(), name='zamboni')
 ]
