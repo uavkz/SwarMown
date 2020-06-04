b78828beb9c30c7dee6c5345b3d442299133c274
Yakunin Kirill <yakunin.k@mail.ru>
2020-06-04 23:08:09 +0600

    feat - add field view basis, bootstrap integrated, header

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -6,4 +6,5 @@ app_name = 'mainapp'
 
 urlpatterns = [
     path('', MownView.as_view(), name="index"),
+    path('add-field/', TemplateView.as_view(template_name="mainapp/add_field.html"), name="add_field"),
 ]
