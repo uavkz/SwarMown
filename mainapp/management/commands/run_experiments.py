cf8d65e403e036c6ec05a5d04b17983b05efa4e1
sanzhar <Sanzharmrz@gmail.com>
2021-01-16 17:26:19 +0600

    feat - rework experiments script with new classes

--- mainapp/management/commands/run_experiments.py
+++ mainapp/management/commands/run_experiments.py
@@ -8,51 +8,54 @@ class Command(BaseCommand):
 
     def run(self):
 
-        from mainapp.services_draw import get_right_edges, get_zigzag_path, get_field, get_grid, generate_stops, \
-            get_extreme_points, when_to_move_forward, best_stop_num, all_pools_flight, final_path_calculations
+        from mainapp.refactored_services import Area
+        from mainapp.to_refactor_services import generate_stops, best_stop_num, all_pools_flight, when_to_move_forward, \
+            final_path_calculations
         import matplotlib.pyplot as plt
         import numpy as np
 
-        field = get_field()
-        grid = get_grid(field, 0.0025)
-        # from geopy.distance import distance
+        perimeter = [
+            [76.84627532958986, 43.21933865889726],
+            [76.84301376342773, 43.23559036068933],
+            [76.84816360473634, 43.25096298148861],
+            [76.87253952026369, 43.25421198192111],
+            [76.88798904418947, 43.23696530585579],
+            [76.88781738281251, 43.22296441400881],
+            [76.87511444091798, 43.21571268816603],
+            [76.86035156250001, 43.21496246040482]
+        ]
 
+        step = 0.0025
+        field_first = Area(perimeter=perimeter, step=step)
         MAX_D = 0.15
         SWARM_POPULATION = 3
         DRONE_TIME = 0.05
         TRUCK_SPEED = 30
-        extremums = get_extreme_points(grid)
+        extremums = field_first.get_extreme_points()
         drones_inits = extremums['bottom_left']
         X_END = extremums['bottom_right'][0]
         Y_END = extremums['top_left'][1]
-        zamboni_path = get_zigzag_path(grid=grid)
-
-        right_edges = get_right_edges(new_coords=zamboni_path)
 
         # First iteration to get the number of flights for each drone in subfields
         truck_path_pool = generate_stops(x_init=drones_inits[0], x_end=X_END,
                                          x_end_km=5, y_const=drones_inits[1], y_init=drones_inits[1],
-                                         y_end=Y_END, y_end_km=3,
-                                         x_const=drones_inits[0], drone_time=DRONE_TIME,
-                                         truck_V=TRUCK_SPEED, grid=grid)
+                                         y_end=Y_END, y_end_km=3, x_const=drones_inits[0], drone_time=DRONE_TIME,
+                                         truck_V=TRUCK_SPEED, direction='horizontal', iters=5)
 
-        total_pathways, truck_path = best_stop_num(truck_path_pool=truck_path_pool,
-                                                   drone_flight=MAX_D,
-                                                   right_edges=right_edges,
-                                                   new_coords=zamboni_path)
+        total_pathways, truck_path = best_stop_num(truck_path_pool, MAX_D, field_first.get_right_edges(),
+                                                   field_first.zamboni_path)
 
-        pathways_num, pathways, pathways_start_end, pool_endings = all_pools_flight(truck_stops=truck_path,
-                                                                                    max_drone_flight=MAX_D,
-                                                                                    right_edges=right_edges,
-                                                                                    path_coords=zamboni_path)
+        pathways_num, pathways, pathways_start_end, pool_endings = all_pools_flight(truck_path, MAX_D,
+                                                                                    field_first.get_right_edges(),
+                                                                                    field_first.zamboni_path)
 
-        drone_lifes = when_to_move_forward(truck_path=truck_path, given_drone_num=SWARM_POPULATION,
-                                           estim_pathes=pathways_num)
-        final_pathes = final_path_calculations(drone_lifes=drone_lifes, path_coords=zamboni_path,
-                                               pool_endings=pool_endings, max_drone_flight=MAX_D)
+        drone_lifes = when_to_move_forward(truck_path, SWARM_POPULATION, pathways_num)
 
-        waypoints = final_pathes.copy()
-        for key, value in final_pathes.items():
+        # Final iteration
+        final_path = final_path_calculations(drone_lifes, field_first.zamboni_path, pool_endings, MAX_D)
+
+        waypoints = final_path.copy()
+        for key, value in final_path.items():
             new_path = list()
             for row in value:
                 for elem in row:
