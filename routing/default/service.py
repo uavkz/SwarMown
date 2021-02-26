5133e8e22d11683dda67538caa1a5bb8900b56c0
Yakunin Kirill <yakunin.k@mail.ru>
2021-02-26 17:27:42 +0600

    feat - routing - more distance awareness

--- routing/default/service.py
+++ routing/default/service.py
@@ -8,7 +8,7 @@ def get_waypoints(grid, drones_init, road, drones):
     zamboni_iterator = iterate_zamboni(grid)
 
     last_point = None
-    for drone in cycle(drones.all()):
+    for drone in cycle(list(drones.all())):
         drone_waypoints = []
         point = None
         total_drone_distance = 0
@@ -16,10 +16,10 @@ def get_waypoints(grid, drones_init, road, drones):
             if point is None:
                 break
             if total_drone_distance == 0 and not last_point:
-                generate_fly_to(drone_waypoints, drones_init, point)
+                total_drone_distance += generate_fly_to(drone_waypoints, drones_init, point)
             if total_drone_distance == 0 and last_point:
                 drone_waypoints.append(last_point)
-                generate_fly_to(drone_waypoints, drones_init, last_point)
+                total_drone_distance += generate_fly_to(drone_waypoints, drones_init, last_point)
             if last_point:
                 total_drone_distance += haversine(last_point, point)
                 if total_drone_distance >= drone.max_distance_no_load:
@@ -27,7 +27,7 @@ def get_waypoints(grid, drones_init, road, drones):
                     break
             last_point = point
             drone_waypoints.append(point)
-        generate_fly_back(drone_waypoints, drones_init)
+        total_drone_distance += generate_fly_back(drone_waypoints, drones_init)
         waypoints.append(drone_waypoints)
         if point is None:
             break
@@ -37,10 +37,15 @@ def get_waypoints(grid, drones_init, road, drones):
 def generate_fly_to(drone_waypoints, drones_init, coord_to):
     drone_waypoints.append(drones_init)
     drone_waypoints.append(coord_to)
+    return haversine(drone_waypoints[-1], drone_waypoints[-2])
 
 
 def generate_fly_back(drone_waypoints, drones_init):
     drone_waypoints.append(drones_init)
+    try:
+        return haversine(drone_waypoints[-1], drone_waypoints[-2])
+    except:
+        return 0
 
 
 def iterate_zamboni(grid):
