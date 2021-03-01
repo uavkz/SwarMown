62c9b86ed929562e3e7553cb7d4aa5d40891fb0e
Yakunin Kirill <yakunin.k@mail.ru>
2021-03-01 20:42:47 +0600

    testing stuff :

--- mainapp/models.py
+++ mainapp/models.py
@@ -6,7 +6,7 @@ class Field(models.Model):
         verbose_name = "Поле"
         verbose_name_plural = "Поля"
 
-    name = models.CharField(max_length=250, unique=True)
+    name = models.CharField(max_length=251, unique=True)
     points_serialized = models.TextField() # Json Serialized [[lat, lon], [lat, lon], ...]
     road_serialized = models.TextField(default=[]) # Json Serialized [[lat, lon], [lat, lon], ...]
 
